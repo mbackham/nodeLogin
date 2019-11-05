77918f6963cc238c2147389b2286256f63300de1
mabo <634264926@qq.com>
2019-11-05 22:16:32 +0800

    second commit

--- model.js
+++ model.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/express-suth',{
+mongoose.connect('mongodb://localhost:27017/express-auth',{
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex:true
 })
 const UserSchema = new mongoose.Schema({
     username:{type:String,unique:true},
-    password:{type:String}
+    password:{
+        type:String,
+        set(val){
+            return require('bcrypt').hashSync(val,10)//加密 值和加密等级
+        }
+    }
 })
 const User= mongoose.model('User',UserSchema)
-//User.db.dropCollection('users')
+//User.db.dropCollection('users')删除
 module.exports={
     User
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const {User}=require('./model')
 const express = require('express')
 
 const app = express()
+const SECRET='asdasdasdasd'
 app.use(express.json())
 app.get('/api/users',async(req,res)=>{
     const users = await User.find()
@@ -17,6 +18,41 @@ app.post('/api/register',async(req,res)=>{
     })
     res.send(user ) 
  
+})
+app.post('/api/login',async(req,res)=>{
+    const user = await User.findOne({
+        username:req.body.username
+    })
+    if(!user){//如果用户没有
+        return res.status(422).send({
+            message:'用户名不存在'
+        })
+    }
+    const isPasswordValid = require('bcrypt').compareSync(
+        req.body.password,//第一个参数密码
+        user.password
+    )
+    if(!isPasswordValid){
+        return res.status(422).send({
+            message:'密码无效'
+        })
+    }
+    //生成token
+    const jwt = require('jsonwebtoken')
+    const token = jwt.sign({
+        id:String(user._id),
+    },SECRET)
+    res.send({
+        user,
+    token//token表示通过登陆locolstorage存下来
+})
+})
+app.get('/api/profile',async(req,res)=>{
+    console.log(req.headers.authorization)
+    return res.send('ok')
+    const user=await User.findById(id)
+
+    res.send(user)
 })
 app.listen(10000,()=>{
     console.log('http://localhost:10000')
